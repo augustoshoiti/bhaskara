3c2b35e6a2067f426b22eb36cf02f1d4279cd4a6
Augusto Shoiti <augustoshoiti@gmail.com>
2023-04-14 18:06:41 -0300

    v1

---
+++ script.js
@@ -0,0 +1,48 @@
+//Verificando se todos os campos estão preenchidos
+function checkInput() {
+    let filled = true;
+    let inputs = document.getElementsByTagName("input");
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === "") {
+            filled = false;
+            break;
+        }
+    }
+    return filled;
+}
+
+let confirmButton = document.querySelector("#confirm");
+confirmButton.disabled = true;
+
+document.querySelector("form").addEventListener("input", function() {
+    confirmButton.disabled = !checkInput();
+});
+
+//Executando os cálculos
+function calcular() {
+    let a, b, c, delta, x1, x2, xv, yv, txt;
+    const tag = document.querySelector(".resp");
+
+    a = document.bhask.a.value;
+    b = document.bhask.b.value;
+    c = document.bhask.c.value;
+
+    delta = Math.pow(b, 2) - 4 * a * c;
+
+    if (delta < 0 ) {
+        window.alert("Delta é invalido, tente outros valores");
+        return;
+    } else {
+        x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        xv = -b / (2 * a);
+        yv = -delta / (4 * a);
+
+        tag.innerHTML = "Delta = " +  delta + "<br>";
+        tag.innerHTML += "x1 = " +  x1 + "<br>";
+        tag.innerHTML += "x2 = " +  x2 + "<br>";
+        tag.innerHTML += "xv = " +  xv + "<br>";
+        tag.innerHTML += "yv = " +  yv + "<br>";
+    }
+}
