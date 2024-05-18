b76ff7960ecc917e184e6de267acb7a0e5b387cb
Augusto Shoiti <augustoshoiti@gmail.com>
2024-05-18 11:09:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const confirmButton = document.querySelector("#confirm");
 const inputs = document.getElementsByTagName("input");
+const tag = document.querySelector(".resp");
 confirmButton.disabled = true;
 
 //Verificando se todos os campos estão preenchidos
@@ -16,11 +17,10 @@ document.querySelector("form").addEventListener("input", () => {
 
 function escreva(string, value) {
     return `<div class="resp-content"><span>${string} = </span><span> ${value} </span></div>`
-};
+}
 
 //Executando os cálculos
 function calcular() {
-    const tag = document.querySelector(".resp");
     if (tag.innerHTML === "") {
         let a, b, c, delta, x1, x2, xv, yv;
 
@@ -28,28 +28,33 @@ function calcular() {
         b = Number(document.bhask.b.value);
         c = Number(document.bhask.c.value);
 
-        delta = b ** 2 - 4 * a * c;
+        if (a != 0) {
+            delta = b ** 2 - 4 * a * c;
 
-        if (isNaN(delta) || delta < 0) {
-            alert("Os valores inseridos são inválidos ou a equação não possui raízes reais.");
+            if (isNaN(delta) || delta < 0) {
+                alert("Os valores inseridos são inválidos ou a equação não possui raízes reais.");
+            } else {
+                x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                xv = -b / (2 * a);
+                yv = -delta / (4 * a);
+    
+                tag.innerHTML +=
+                    escreva('Delta', delta) +
+                    ((x1 === x2)? escreva('x1 = x2', x1) : escreva('x1', x1) + escreva('x2', x2)) +
+                    escreva('xv', xv) +
+                    escreva('yv', yv);
+            }
         } else {
-            x1 = (-b + Math.sqrt(delta)) / (2 * a);
-            x2 = (-b - Math.sqrt(delta)) / (2 * a);
-            xv = -b / (2 * a);
-            yv = -delta / (4 * a);
-
-            tag.innerHTML +=
-                escreva('Delta', delta) +
-                ((x1 === x2)? escreva('x1 = x2', x1) : escreva('x1', x1) + escreva('x2', x2)) +
-                escreva('xv', xv) +
-                escreva('yv', yv);
-        };
-    };
-};
+            alert("'A' não pode ser 0.");
+        }
+    } else {
+        alert("Para realizar outro cálculo clique em 'Refazer', essa ação fará com que a resposta atual seja apagada.");
+    }
+}
 
 //Zerando o elemento .resp e desabilitando o botão #confirm
 function refazer() {
-    const tag = document.querySelector(".resp");
     tag.innerHTML = "";
     confirmButton.disabled = true;
-};
+}
